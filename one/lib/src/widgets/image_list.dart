21730b932d93dfb2b09001f9d3ae5e97c8ca0000
Varun Amachi <varunamachi@gmail.com>
2018-08-27 20:36:33 +0530

    Pix done...

--- one/lib/src/widgets/image_list.dart
+++ one/lib/src/widgets/image_list.dart
@@ -14,9 +14,30 @@ class ImageList extends StatelessWidget {
         itemBuilder: (BuildContext context, int index) {
           final img = images[index];
           if (img != null) {
-            return Text("hello ${img.url}");
+            return buildImage(img);
           }
         },
+    );
+  }
+
+  Widget buildImage(ImageModel img) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 10.0),
+            child: Image.network(img.url),
+          ),
+          Text(img.title),
+        ],
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+        ),
+      ),
     );
   }
 }
